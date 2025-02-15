aed0199bfec8dd2ad59d7cc4f9d2f5ab3c0f14e5
ElliotMonde <elliotphua@gmail.com>
2025-02-15 14:53:20 +0800

    get_students: fixed bug: specified default parameter value as None when no earliest and latest semesters specified. shifted backend files to backend folder.

--- teachers_service.py
+++
@@ -1,9 +0,0 @@
-def get_teacher(cur, teacher_id: int):
-    if teacher_id is None:
-        return {"error" : "No teacher id provided."}
-    sql_str = """
-    SELECT name FROM teachers WHERE id = %s
-    """
-    cur.execute(sql_str, (teacher_id,))
-    res = cur.fetchall()
-    return res
