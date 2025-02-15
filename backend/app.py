5d879d30f21e6f765e49ccf01a569055996ba72a
ElliotMonde <elliotphua@gmail.com>
2025-02-15 17:05:58 +0800

    add railway.yaml for deployment on railway. removed Procfile.

--- backend/app.py
+++ backend/app.py
@@ -92,6 +92,6 @@ def on_app_close() -> None:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
 atexit.register(on_app_close)
