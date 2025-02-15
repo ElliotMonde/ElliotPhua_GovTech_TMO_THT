4d7112aece3966d990c260b18e38e49e39382716
ElliotMonde <elliotphua@gmail.com>
2025-02-16 00:33:12 +0800

    added table component with filters and single=selection.

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,47 +1,55 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
 import axios from "axios";
 
 export default function Table() {
   const [studentsData, setStudentsData] = useState([]);
-  const test = useRef();
-  const rerender = React.useReducer(() => ({}), {})[1];
-  const getStudentsUrl = "https://elliotphuagovtechtmotht-production.up.railway.app/students";
-
+  const [checkboxSelection, setCheckBoxSelection] = useState(undefined);
+  const baseUrl = "https://elliotphuagovtechtmotht-production.up.railway.app/";
+  const getStudentsUrl = baseUrl + "students";
+  const getTeachersUrl = baseUrl + "teachers";
+  const updateTeacherUrl = baseUrl + `student/{}`;
+  // add checkboxes to allow one selection -> and dropdown to select teacher and button to submit update
+  // add draggable range for start and end semester
+  // readme -> assumptions
   useEffect(() => {
     axios
       .get(getStudentsUrl)
       .then(response => {
         setStudentsData(response.data);
-        console.log(response);
       })
       .catch((error) => {
-        console.error("There was an error fetching the data!", error);
+        console.error("There was an error fetching students data", error);
       });
   }, [])
+
+  const columns = [
+    { field: 'student_id', headerName: 'ID', width: 75, flex: 1, sortable: true },
+    { field: 'student_name', headerName: 'Student Name', width: 240, flex: 2, sortable: true },
+    { field: 'teacher_name', headerName: 'Teacher', width: 240, flex: 2, sortable: true },
+    { field: 'cumulative_GPA', headerName: 'Cumulative GPA', width: 200, flex: 2, sortable: true }
+  ];
+
+  const paginationModel = { page: 0, pageSize: 10 };
+
   return (
-    <div>
-      {studentsData.length > 0 ?
-        <table>
-          <thead>
-            <tr>
-              <th>Student Name</th>
-              <th>Teacher</th>
-              <th>Cumulative GPA</th>
-            </tr>
-          </thead>
-          <tbody>
-            {studentsData.map((student, ind) => (
-              <tr key={ind}>
-                <td>{student.student_name}</td>
-                <td>{student.teacher_name}</td>
-                <td>{student.cumulative_GPA}</td>
-              </tr>
-            ))
-            }
-          </tbody>
-          <tfoot>
-          </tfoot>
-        </table>
-        : <p>loading...</p>}
-    </div>)
+    <>
+      <Paper>
+        <DataGrid
+          getRowId={(row) => row.student_id}
+          rows={studentsData}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[10, 5]}
+          checkboxSelection
+          disableMultipleRowSelection
+          onRowSelectionModelChange={(e) => { setCheckBoxSelection(e[0]) }}
+        />
+      </Paper>
+      <div>
+        Currently Selected:
+        {studentsData.length > 0 && checkboxSelection != undefined ? studentsData[checkboxSelection - 1]["student_name"] : "None" }
+      </div>
+    </>)
 }
