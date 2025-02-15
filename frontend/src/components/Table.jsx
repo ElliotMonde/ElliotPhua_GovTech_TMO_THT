11aa0d995786dbf20965d1ab673520733c0ffe09
ElliotMonde <elliotphua@gmail.com>
2025-02-15 18:38:14 +0800

    initialised basic table element that is populated by student data fetched from students api.

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from "axios";
 
 export default function Table() {
   const [studentsData, setStudentsData] = useState([]);
-  const [error, setError] = useState(null);
+  const test = useRef();
   const rerender = React.useReducer(() => ({}), {})[1];
   const getStudentsUrl = "https://elliotphuagovtechtmotht-production.up.railway.app/students";
 
@@ -15,13 +15,33 @@ export default function Table() {
         console.log(response);
       })
       .catch((error) => {
-        setError("Failed to fetch students data");
         console.error("There was an error fetching the data!", error);
       });
   }, [])
   return (
     <div>
-      { studentsData }
-    </div>
-  )
+      {studentsData.length > 0 ?
+        <table>
+          <thead>
+            <tr>
+              <th>Student Name</th>
+              <th>Teacher</th>
+              <th>Cumulative GPA</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentsData.map((student, ind) => (
+              <tr key={ind}>
+                <td>{student.student_name}</td>
+                <td>{student.teacher_name}</td>
+                <td>{student.cumulative_GPA}</td>
+              </tr>
+            ))
+            }
+          </tbody>
+          <tfoot>
+          </tfoot>
+        </table>
+        : <p>loading...</p>}
+    </div>)
 }
